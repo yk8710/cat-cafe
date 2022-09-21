792e1fd523180a8e6da5e7b3e65583450945ed27
yk8710 <y.kaneko198710@gmail.com>
2022-09-21 09:39:51 +0900

    フォームリクエスト

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
@@ -12,14 +13,8 @@ class ContactController extends Controller
         return view('contact.index');
     }
 
-    function sendMail(Request $request) {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'name_kana' => ['required', 'string', 'max:255', 'regex:/^[ァ-ロワンヴー]*$/u'],
-            'phone' => ['nullable', 'regex:/^0(\d-?\d{4}|\d{2}-?\d{3}|\d{3}-?\d{2}|\d{4}-?\d|\d0-?\d{4})-?\d{4}$/'],
-            'email' => ['required', 'email'],
-            'body' => ['required', 'string', 'max:2000'],
-        ]);
+    function sendMail(ContactRequest $request) {
+        $validated = $request->validated();
     
         // これ以降の行は入力エラーがなかった場合のみ実行されます
         // 登録処理(実際はメール送信などを行う)
@@ -31,4 +26,5 @@ class ContactController extends Controller
     {
         return view('contact.complete');
     }
+    
 }
