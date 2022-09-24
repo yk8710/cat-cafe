51f407920676b752e2988fadaa4424c188bd6529
yk8710 <y.kaneko198710@gmail.com>
2022-09-24 15:58:13 +0900

    authミドルウエア

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::post('/contact', [ContactController::class, 'sendMail']);
 Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
 
 // ブログ
-Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index');
+Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
 Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
 Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
 Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
