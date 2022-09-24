cfef1a6758598ecb29e74b2ec1d21206f3c6a3e4
yk8710 <y.kaneko198710@gmail.com>
2022-09-24 16:03:41 +0900

    guestミドルウエア

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::get('/admin/users/create', [UserController::class, 'create'])->name('admi
 Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
 
 // ログイン用ルート
-Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login')->middleware('guest');
 Route::post('/admin/login', [AuthController::class, 'login']);
 
 // ログアウト用ルート
